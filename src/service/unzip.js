45bb35919269479abf50c88816d7496b5a7e5f5c
NVC <nguyencuong21520@gmail.com>
2022-12-23 20:28:43 +0700

    update

--- src/service/unzip.js
+++ src/service/unzip.js
@@ -1,8 +1,8 @@
 // const decompress = require('decompress');
  
-// decompress('../scorm.zip', 'dist').then(files => {
-//     console.log('done!');
-// });
+decompress('../scorm.zip', 'dist').then(files => {
+    console.log('done!');
+});
 
-var rimraf = require("rimraf");
-rimraf("dist/scorm", function () { console.log("done"); });
+// var rimraf = require("rimraf");
+// rimraf("dist/scorm", function () { console.log("done"); });
