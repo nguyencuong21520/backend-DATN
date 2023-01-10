8b65573187a495304af9a8c23afcbf446fed3323
NVC <nguyencuong21520@gmail.com>
2023-01-10 23:52:36 +0700

    hihiih

--- index.ts
+++ index.ts
@@ -1,16 +1,26 @@
 import { config } from "dotenv";
 import { App } from "./src/app";
 
+const http = require("http");
+const express = require("express");
+const socketIo = require("socket.io");
+const app_ = express();
+const sever = http.createServer(app_);
+const io = socketIo(sever, {
+  cors: {
+    origin: "*",
+  },
+});
 
+io.on("connection", () => {
+  console.log("connection hello");
+});
 
-
-config()
+config();
 
 const NAME = "Learn TLU APIs";
 const PORT = parseInt(process.env.BACKEND_PORT as string) || 8000;
 const app = new App(NAME, PORT);
 
-
 // app.publicFile()
-app.listen()
-
+app.listen();
